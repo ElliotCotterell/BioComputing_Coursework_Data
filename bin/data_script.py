b238b5bb0a55545b4c4ca51bbeb95bef97bed2b2
ElliotCotterell <elliotcotterell@gmail.com>
2023-04-01 14:01:20 +0100

    Top Level Script added
    Deliverable 1 complete.Data Files are being read into python and are iterable.

--- bin/data_script.py
+++ bin/data_script.py
@@ -1,1 +1,34 @@
 # Python scipt for data manipluation 
+# deliverable 1
+#Read in the dog_breeds.fa file and the mystery.fa sequence.
+# utilise the os liberary
+import os
+
+# import SeqIO from Biopython
+
+from Bio import SeqIO
+
+
+# Set filepath
+
+reference_file_path = os.path.join("data", "dog_breeds.fa")
+
+
+# Open the file using Biopython's SeqIO module
+with open(reference_file_path, "r") as file:
+    # Parse the sequences in the file
+    for record in SeqIO.parse(file, "fasta"):
+        # Print the header and sequence data for each sequence
+        print(record.description)
+print("These are the reference records")
+        # print(record.seq)
+
+mystery_file_path = os.path.join("data", "mystery.fa")
+
+# Open the file using Biopython's SeqIO module
+with open(mystery_file_path, "r") as file:
+    # Parse the sequences in the file
+    for record in SeqIO.parse(file, "fasta"):
+        # Print the header and sequence data for each sequence
+        print(record.description)
+        print("Theis is the mystery record")
